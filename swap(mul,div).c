1f32995fe7d48e9c5d23cc5421df0186394d8f08
24211a05dy <24211a05dy@bvrit.ac.in>
2025-07-26 19:20:57 +0530

    Update and rename README.md to swap(mul,div).c

---
+++ swap(mul,div).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    int a, b;
+    clrscr();
+    printf("Enter the values of a and b: ");
+    scanf("%d %d", &a, &b);
+    printf("Before swapping a=%d\tb=%d\n", a, b);
+    // logic begins
+    a = a * b;
+    b = a / b;
+    a = a / b;
+    printf("After swapping a=%d\tb=%d\n", a, b);
+    return 0;
+}
