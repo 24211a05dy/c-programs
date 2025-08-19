8eaa9f11479ab58ee7315b5d74a3f77da0837189
24211a05dy <24211a05dy@bvrit.ac.in>
2025-08-19 21:19:09 +0530

    Create tower _of_hanoi_using_recursion.c

---
+++ tower _of_hanoi_using_recursion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void toh(char src, char aux, char dst, int n);
+void main(){
+  int n;
+  printf("Enter no of disks :"); 
+  scanf("%d,&n);
+  printf(" steps to solve tower of hanoi with %d disks :\n",n);
+  toh('A','B','C',n);
+}
+void toh(char src, char aux, char dst, int n){
+  if(n==0){
+     return;
+  }
+  else{
+    toh(src,dst,aux,n-1);
+    printf("Move %d disk from %c --> %c",n,src,dst);
+    toh(aux,src,dst,n-1);
+  }
+}
+
